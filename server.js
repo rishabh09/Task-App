ae65af5bd5f5c01f22cb09be3881a48f5923b960
Rishabh Mishra <mishrarishabh865@gmail.com>
2016-12-25 12:40:13 +0530

    Removed host and db name with env variable

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ const me_endpoint_base_url = 'https://graph.accountkit.com/v1.0/me'
 const token_exchange_base_url = 'https://graph.accountkit.com/v1.0/access_token'
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'taskapp'
+  database: process.env.DB_DBNAME
 })
 
 connection.connect(function (err) {
