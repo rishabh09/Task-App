7f5ed5cc1999026e20555734075bcd6c62225e23
Rishabh Mishra <mishrarishabh865@gmail.com>
2016-12-27 10:35:25 +0530

    Update form post request fixed

--- public/components/Update.jsx
+++ public/components/Update.jsx
@@ -29,7 +29,7 @@ const Update = React.createClass({
     }
     return (
       <div className='row'>
-        <form>
+        <form name="Update-Form" method="post" action="updateprofile" >
           <div className='updateForm'>
            <div id="fname_div" className="formRows">
             <label>
